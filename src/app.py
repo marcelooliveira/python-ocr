5544ffcd5b3bc653853124a00fbe4d7661ea98dd
marcelooliveira <mclricardo@gmail.com>
2023-07-14 19:55:02 -0300

    0000 flask

--- src/app.py
+++ src/app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from quickstart import process_ocr
+from json import dumps
 
 app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        source_image = "https://learn.microsoft.com/azure/cognitive-services/computer-vision/media/quickstarts/presentation.png"
+        report = process_ocr(source_image)
+        return report
 
 api.add_resource(HelloWorld, '/')
 
--- src/quickstart.py
+++ src/quickstart.py
@@ -1,47 +1,48 @@
 import os
 import azure.ai.vision as sdk
 
-service_options = sdk.VisionServiceOptions(os.environ["VISION_ENDPOINT"],
+def process_ocr(source_image):
+    service_options = sdk.VisionServiceOptions(os.environ["VISION_ENDPOINT"],
                                            os.environ["VISION_KEY"])
 
-vision_source = sdk.VisionSource(
-    url="https://learn.microsoft.com/azure/cognitive-services/computer-vision/media/quickstarts/presentation.png")
+    vision_source = sdk.VisionSource(url=source_image)
 
-analysis_options = sdk.ImageAnalysisOptions()
+    analysis_options = sdk.ImageAnalysisOptions()
 
-analysis_options.features = (
-    sdk.ImageAnalysisFeature.CAPTION |
-    sdk.ImageAnalysisFeature.TEXT
-)
+    analysis_options.features = (
+        sdk.ImageAnalysisFeature.CAPTION |
+        sdk.ImageAnalysisFeature.TEXT
+    )
 
-analysis_options.language = "en"
+    analysis_options.language = "en"
 
-analysis_options.gender_neutral_caption = True
+    analysis_options.gender_neutral_caption = True
 
-image_analyzer = sdk.ImageAnalyzer(service_options, vision_source, analysis_options)
+    image_analyzer = sdk.ImageAnalyzer(service_options, vision_source, analysis_options)
 
-result = image_analyzer.analyze()
+    result = image_analyzer.analyze()
 
-if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+    report = ""
 
-    if result.caption is not None:
-        print(" Caption:")
-        print("   '{}', Confidence {:.4f}".format(result.caption.content, result.caption.confidence))
+    if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+        if result.caption is not None:
+            report = report + "\r\n Caption:"
+            report = report + "\r\n   '{}', Confidence {:.4f}".format(result.caption.content, result.caption.confidence)
 
-    if result.text is not None:
-        print(" Text:")
-        for line in result.text.lines:
-            points_string = "{" + ", ".join([str(int(point)) for point in line.bounding_polygon]) + "}"
-            print("   Line: '{}', Bounding polygon {}".format(line.content, points_string))
-            for word in line.words:
-                points_string = "{" + ", ".join([str(int(point)) for point in word.bounding_polygon]) + "}"
-                print("     Word: '{}', Bounding polygon {}, Confidence {:.4f}"
-                      .format(word.content, points_string, word.confidence))
+        if result.text is not None:
+            report = report + "\r\n Text:"
+            for line in result.text.lines:
+                points_string = "{" + ", ".join([str(int(point)) for point in line.bounding_polygon]) + "}"
+                report = report + "\r\n   Line: '{}', Bounding polygon {}".format(line.content, points_string)
+                for word in line.words:
+                    points_string = "{" + ", ".join([str(int(point)) for point in word.bounding_polygon]) + "}"
+                    report = report + "\r\n     Word: '{}', Bounding polygon {}, Confidence {:.4f}".format(word.content, points_string, word.confidence)
 
-else:
+    else:
+        error_details = sdk.ImageAnalysisErrorDetails.from_result(result)
+        report = report + "\r\n Analysis failed."
+        report = report + "\r\n   Error reason: {}".format(error_details.reason)
+        report = report + "\r\n   Error code: {}".format(error_details.error_code)
+        report = report + "\r\n   Error message: {}".format(error_details.message)
 
-    error_details = sdk.ImageAnalysisErrorDetails.from_result(result)
-    print(" Analysis failed.")
-    print("   Error reason: {}".format(error_details.reason))
-    print("   Error code: {}".format(error_details.error_code))
-    print("   Error message: {}".format(error_details.message))
+    return report
