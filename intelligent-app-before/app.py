506490805bedc9b1a9cf4657a6e838bce1b98b7c
marcelooliveira <mclricardo@gmail.com>
2023-07-29 15:53:01 -0300

    0000 formatted app.py

--- intelligent-app-before/app.py
+++ intelligent-app-before/app.py
@@ -1,4 +1,3 @@
-"""Main application module."""
 import os
 import json
 
@@ -7,45 +6,40 @@ from flask_restful import Resource, Api
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__,
-            static_url_path='',
-            static_folder='static/files')
+      static_url_path='',
+      static_folder='static/files')
 
 api = Api(app)
 
-app = Flask(__name__)
+app = Flask(__name__) 
 app.config['UPLOAD_FOLDER'] = 'files'
 
 api = Api(app)
 
 class UploadHandler(Resource):
-    """Handles file upload."""
 
-    def allowed_file(self, filename):
-        """Determines whether a file extension is allowed for upload."""
-        return '.' in filename and \
-            filename.rsplit('.', 1)[1].lower() in {'png'}
+  def allowed_file(self, filename):
+    return '.' in filename and \
+      filename.rsplit('.', 1)[1].lower() in {'png'}
 
-    def post(self):
-        """Handles HTTP POST requests."""
+  def post(self):
+    form = request.form.to_dict()
 
-        # Get Text type fields
-        form = request.form.to_dict()
+    if 'file' not in request.files:
+      return json.dumps({ "success": False, "error": "No file part"})
 
-        if 'file' not in request.files:
-            return json.dumps({ "success": False, "error": "No file part"})
+    file = request.files.get("file")
+    if file and self.allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      upload_folder = "static/files"
+      if not os.path.exists(upload_folder):
+        os.makedirs(upload_folder)
+      local_file_path = os.path.join(upload_folder, filename)
+      file.save(local_file_path)
 
-        file = request.files.get("file")
-        if file and self.allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            upload_folder = "static/files"
-            if not os.path.exists(upload_folder):
-                os.makedirs(upload_folder)
-            local_file_path = os.path.join(upload_folder, filename)
-            file.save(local_file_path)
-
-            return f"File {filename} uploaded successfully to folder: {upload_folder}"
+      return f"File {filename} uploaded successfully to folder: {upload_folder}"
 
 api.add_resource(UploadHandler, "/")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
