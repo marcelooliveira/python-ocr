65244b69f4c02f64d7b90f104ceb4efad015baa8
marcelooliveira <mclricardo@gmail.com>
2023-08-06 16:31:23 -0300

    saving analysis_result json file

--- part2/intelligent-app-after-pt2/ocr_helper.py
+++ part2/intelligent-app-after-pt2/ocr_helper.py
@@ -1,3 +1,4 @@
+import json
 import os
 from statistics import median
 from decimal import Decimal
@@ -24,19 +25,62 @@ def process_ocr(source_image):
 
   result = image_analyzer.analyze()
 
+  upload_folder = "static/files"
+
+  base_file_name, ext = source_image.split('.')
+  result_file = '{}.{}'.format(base_file_name, 'json')
+
+  analysis_result = { "file_name": result_file }
+
   string_list = []
 
   if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+
+    if result.caption is not None:
+        print(" Caption:", flush=True)
+        print("   '{}', Confidence {:.4f}".format(result.caption.content, result.caption.confidence), flush=True)
+        analysis_result["caption"] = { 
+                                      "content": result.caption.content,
+                                      "confidence": result.caption.confidence
+                                      }
+        
+        analysis_result["text"] = {
+                                      "lines": []
+                                  }
+
     if result.text is not None:
+      print(" Text:", flush=True)      
       for line in result.text.lines:
         points_string = ("{" +
         ", ".join([str(int(point)) for point in line.bounding_polygon]) + 
         "}")
+        print("   Line: '{}', Bounding polygon {}".format(line.content, points_string), flush=True)
+        line_result = {
+                        "content": line.content,
+                        "bounding_polygon": line.bounding_polygon,
+                        "words": [],
+                      }
+
         for word in line.words:
+          word_result = {
+                          "content": word.content,
+                          "bounding_polygon": word.bounding_polygon,
+                          "confidence": word.confidence
+                        }
           points_string = ("{" +
                    ", ".join([str(int(p)) for p in word.bounding_polygon]) +
                    "}")
+          print("     Word: '{}', Bounding polygon {}, Confidence {:.4f}"
+                .format(word.content, points_string, word.confidence), flush=True)
           string_list.append(word.content)
+          line_result["words"].append(word_result)
+
+        analysis_result["text"]["lines"].append(line_result)
+
+      local_file_path = result_file
+      results_file = open(local_file_path, "w")
+      results_file.write(json.dumps(analysis_result))
+      results_file.close()
 
   else:
     error_details = sdk.ImageAnalysisErrorDetails.from_result(result)
