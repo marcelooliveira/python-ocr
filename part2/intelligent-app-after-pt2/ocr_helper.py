d2b7a005659957003c9150030a390eda3afeb49b
marcelooliveira <mclricardo@gmail.com>
2023-08-10 23:43:41 -0300

    Part 1 and Part 2 - code cleanup

--- part2/intelligent-app-after-pt2/ocr_helper.py
+++ part2/intelligent-app-after-pt2/ocr_helper.py
@@ -26,18 +26,42 @@ def process_ocr(source_image):
 
   result = image_analyzer.analyze()
 
-  base_file_name, ext = source_image.split('.')
-  result_file = '{}.{}'.format(base_file_name, 'json')
+  ocr_result = get_ocr_result(result)
+  analysis_result = get_image_analysis_result(result, source_image)
+  ocr_result["analysis_result"] = analysis_result
+  inserted_id = insert_analysis(ocr_result["analysis_result"])
+  insert_aggregate_result(inserted_id, ocr_result["aggregate_result"])
+  return ocr_result
 
-  analysis_result = { "file_name": result_file }
-
+def get_ocr_result(result):
   string_list = []
 
-  if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+  if result.reason != sdk.ImageAnalysisResultReason.ANALYZED:
+    return sdk.ImageAnalysisErrorDetails.from_result(result)
+  else:
+    if result.text is not None:
+      for line in result.text.lines:
+        for word in line.words:
+          string_list.append(word.content)
 
+  number_list = convert_to_decimal_list(string_list)
+
+  aggregate_result = aggregate_operations(number_list)
+
+  return {
+    "aggregate_result": aggregate_result,
+    "numbers_read": string_list
+  }
+
+def get_image_analysis_result(result, source_image):
+  
+  analysis_result = { }
+  string_list = []
+
+  if result.reason != sdk.ImageAnalysisResultReason.ANALYZED:
+    return sdk.ImageAnalysisErrorDetails.from_result(result)
+  else:
     if result.caption is not None:
-        print(" Caption:", flush=True)
-        print("   '{}', Confidence {:.4f}".format(result.caption.content, result.caption.confidence), flush=True)
         analysis_result["caption"] = { 
                                       "content": result.caption.content,
                                       "confidence": result.caption.confidence
@@ -48,12 +72,7 @@ def process_ocr(source_image):
                                   }
 
     if result.text is not None:
-      print(" Text:", flush=True)      
       for line in result.text.lines:
-        points_string = ("{" +
-        ", ".join([str(int(point)) for point in line.bounding_polygon]) + 
-        "}")
-        print("   Line: '{}', Bounding polygon {}".format(line.content, points_string), flush=True)
         line_result = {
                         "content": line.content,
                         "bounding_polygon": line.bounding_polygon,
@@ -66,35 +85,19 @@ def process_ocr(source_image):
                           "bounding_polygon": word.bounding_polygon,
                           "confidence": word.confidence
                         }
-          points_string = ("{" +
-                   ", ".join([str(int(p)) for p in word.bounding_polygon]) +
-                   "}")
-          print("     Word: '{}', Bounding polygon {}, Confidence {:.4f}"
-                .format(word.content, points_string, word.confidence), flush=True)
           string_list.append(word.content)
           line_result["words"].append(word_result)
 
-        analysis_result["text"]["lines"].append(line_result)
         document_id = os.path.basename(source_image).rsplit('.', 1)[0]
+        base_file_name, ext = source_image.split('.')
+        result_file = '{}.{}'.format(base_file_name, 'json')
+
+        analysis_result["file_name"] = result_file
+        analysis_result["text"]["lines"].append(line_result)
         analysis_result["id"] = document_id
         analysis_result["partitionKey"] = "Partition1"
 
-      local_file_path = result_file
-      results_file = open(local_file_path, "w")
-      results_file.write(json.dumps(analysis_result))
-      results_file.close()
-
-      inserted_id = insert_analysis(analysis_result)
-
-      number_list = convert_to_decimal_list(string_list)
-
-      aggregate_result = aggregate_operations(number_list)
-
-      insert_aggregate_result(inserted_id, aggregate_result)
-      return aggregate_result
-
-  else:
-    return sdk.ImageAnalysisErrorDetails.from_result(result)
+      return analysis_result
 
 def insert_aggregate_result(inserted_id, aggregate_result):
     db_helper = CosmosDBHelper()
